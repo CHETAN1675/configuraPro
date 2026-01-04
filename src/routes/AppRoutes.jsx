3558551af563a575c34661353f35a814469a41f1
CHETAN1675 <mahajanchetan001@gmail.com>
2026-01-05 00:28:00 +0530

    Added protected routes based on authentication state

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,14 +2,31 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Auth from "../pages/Auth";
 import Dashboard from "../pages/Dashboard";
 import Configurator from "../pages/Configurator";
+import ProtectedRoute from "./ProtectedRoute";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/auth" element={<Auth />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/configurator" element={<Configurator />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/configurator"
+          element={
+            <ProtectedRoute>
+              <Configurator />
+            </ProtectedRoute>
+          }
+        />
 
         <Route path="/" element={<Navigate to="/auth" replace />} />
         <Route path="*" element={<Navigate to="/auth" replace />} />
